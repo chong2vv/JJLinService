37fba3f93aaf8a354e764d66407c1cca3dff0f4f
wangyuandong <chong2vv@163.com>
2022-09-17 23:30:10 +0800

    添加用户端消息接口

---
+++ src/main/java/com/ry/time/client/service/impl/ClientClientMessageServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ry.time.client.service.impl;
+
+import com.ry.time.admin.dao.MessageDao;
+import com.ry.time.admin.model.entity.Message;
+import com.ry.time.client.service.ClientMessageService;
+import com.ry.time.common.util.DateUtil;
+import com.ry.time.common.util.NumberUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * MessageService实现类
+ *
+ * @author wangyuandong
+ * @date 2022/9/17
+ *
+ **/
+
+@Service
+@RequiredArgsConstructor
+public class ClientClientMessageServiceImpl implements ClientMessageService {
+    private final MessageDao messageDao;
+
+    @Override
+    public int createMessage(Message message) {
+        message.setId(NumberUtil.genUid());
+        message.setCreateTime(DateUtil.getCurrentDateTimeStr());
+        return messageDao.insert(message);
+    }
+}
