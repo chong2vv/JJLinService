e8904ea889c8f00f5fb4f3dd0d107b64a6a1e663
chong2vv <chong2vv@gmail.com>
2023-04-15 18:49:07 +0800

    新增分类

---
+++ src/main/java/com/yd/blog/admin/model/vo/CategoriesPagerRequestVO.java
@@ -0,0 +1,22 @@
+package com.yd.blog.admin.model.vo;
+
+import com.yd.blog.common.model.PagerRequestVO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 分类请求信息
+ *
+ * @author wangyuandong
+ * @date 2022/9/10
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CategoriesPagerRequestVO extends PagerRequestVO {
+
+    /**
+     * 状态
+     */
+    private Integer status;
+
+}
---
+++ src/main/java/com/yd/blog/admin/service/impl/CategoriesServiceImpl.java
@@ -0,0 +1,65 @@
+package com.yd.blog.admin.service.impl;
+
+import com.yd.blog.admin.dao.CategoriesDao;
+import com.yd.blog.admin.model.entity.Categories;
+import com.yd.blog.admin.model.vo.CategoriesPagerRequestVO;
+import com.yd.blog.admin.service.CategoriesService;
+import com.yd.blog.common.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 文章分类(Categories)表服务实现类
+ *
+ * @author makejava
+ * @since 2023-04-15 18:21:10
+ */
+@Service
+@RequiredArgsConstructor
+public class CategoriesServiceImpl implements CategoriesService {
+    @Resource
+    private CategoriesDao categoriesDao;
+
+    @Override
+    public List<Categories> getCategoriesList(CategoriesPagerRequestVO categoriesPagerRequestVO) {
+        categoriesPagerRequestVO.initPager();
+        return CategoriesDao.queryPager(categoriesPagerRequestVO);
+    }
+
+    @Override
+    public int count(Integer status) {
+        return categoriesDao.count(status);
+    }
+
+    @Override
+    public Categories queryByCategoriesId(Integer id) {
+        return categoriesDao.queryById(id);
+    }
+
+    @Override
+    public boolean existByCategoriesId(Integer id) {
+        Categories categories = categoriesDao.queryById(id);
+        return categories != null;
+    }
+
+    @Override
+    public void update(Categories categories) {
+        categoriesDao.update(categories);
+    }
+
+    @Override
+    public void updateStatus(Categories categories) {
+        categoriesDao.updateStatus(categories);
+    }
+
+    @Override
+    public Categories create(Categories categories) {
+        categories.setCreateTime(DateUtil.getCurrentDateTimeStr());
+        categoriesDao.insert(categories);
+        return categories;
+    }
+}
